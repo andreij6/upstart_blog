882563d8f9df353ec889a8baf8674c7c9d9ee3ec
Andre I. Jones <andreij6@gmail.com>
2014-02-10 19:46:23 -0600

    first heroku push

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def message
+    "Help Andre' Get into MakerSquare and become a Web Developer.".gsub(" ","%20")
+  end
+  
+  def tweet(message)
+      '<a href="https://twitter.com/intent/tweet?button_hashtag=Upstart&text='+ message + '" class="twitter-hashtag-button" data-lang="en" data-related="s3nojA">Tweet</a>'
+  end
 end
